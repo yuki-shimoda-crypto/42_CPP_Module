f306f27d7258e27858c8da7e9f10a55117ef6349
yuki-shimoda <yuki-shimoda@protonmail.com>
2023-12-01 16:04:37 +0900

    [Module: 01] [Ex: 03] add const
    Detailed description:
    
    Testing:

--- CPP_Module_01/ex03/Weapon.cpp
+++ CPP_Module_01/ex03/Weapon.cpp
@@ -3,6 +3,6 @@
 
 Weapon::Weapon(std::string type) : _type(type) {}
 
-const std::string &Weapon::getType() { return (this->_type); }
+const std::string &Weapon::getType() const { return (this->_type); }
 
 void Weapon::setType(std::string type) { this->_type = type; }
--- CPP_Module_01/ex03/Weapon.hpp
+++ CPP_Module_01/ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 class Weapon {
 public:
   Weapon(std::string type);
-  const std::string &getType();
+  const std::string &getType() const;
   void setType(std::string type);
 
 private:
