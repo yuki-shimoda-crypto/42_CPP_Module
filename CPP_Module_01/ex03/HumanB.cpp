b64eb00a4db9f929727b914d359d185803abe376
yuki-shimoda <yuki-shimoda@protonmail.com>
2023-12-01 17:14:24 +0900

    [Module: 01] [Ex: 03] fix segv HumanB
    Detailed description:
    
    Testing:

--- CPP_Module_01/ex03/HumanB.cpp
+++ CPP_Module_01/ex03/HumanB.cpp
@@ -7,8 +7,10 @@ void HumanB::attack(void) const {
   if (_weapon == NULL) {
   	std::cout << this->_name << " dose not have a weapon." << std::endl;
   }
-  std::cout << this->_name << " attacks with their " << this->_weapon->getType()
-            << std::endl;
+  else {
+    std::cout << this->_name << " attacks with their " << this->_weapon->getType()
+              << std::endl;
+  }
 }
 
 void HumanB::setWeapon(Weapon &weapon) { this->_weapon = &weapon; }
