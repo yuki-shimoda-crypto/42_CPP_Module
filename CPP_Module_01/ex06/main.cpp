67457b7f65be0da9904177290abb99bc325b005c
yuki-shimoda <yuki-shimoda@protonmail.com>
2023-12-01 15:26:26 +0900

    [Module: 01] [Ex: 06] fix main.cpp
    Detailed description:
    
    Testing:

--- CPP_Module_01/ex06/main.cpp
+++ CPP_Module_01/ex06/main.cpp
@@ -11,15 +11,4 @@ int main(void) {
   harl.complain("DEBUG");
   std::cout << std::endl;
 
-  std::cout << "input INFO" << std::endl;
-  harl.complain("INFO");
-  std::cout << std::endl;
-
-  std::cout << "input WARNING" << std::endl;
-  harl.complain("WARNING");
-  std::cout << std::endl;
-
-  std::cout << "input ERROR" << std::endl;
-  harl.complain("ERROR");
-  std::cout << std::endl;
 }
