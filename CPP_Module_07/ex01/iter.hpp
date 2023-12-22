ca4952f2fec8b6997bf7af0fc275cca78378271f
yuki-shimoda <yuki-shimoda@protonmail.com>
2023-12-22 11:04:49 +0900

    [Module: 07] [Ex: 01] Fix with const withcout const
    Detailed description:
    
    Testing:

--- CPP_Module_07/ex01/iter.hpp
+++ CPP_Module_07/ex01/iter.hpp
@@ -5,10 +5,32 @@
 #include <string>
 
 template <class T>
+void iter(T *array, size_t length, void (*func)(T &element)) {
+  for (size_t i = 0; i < length; i++) {
+    func(array[i]);
+  }
+}
+
+template <class T>
 void iter(T *array, size_t length, void (*func)(const T &element)) {
   for (size_t i = 0; i < length; i++) {
     func(array[i]);
   }
 }
+
+// This pattern is not possible
+// template <class T>
+// void iter(const T *array, size_t length, void (*func)(T &element)) {
+//   for (size_t i = 0; i < length; i++) {
+//     func(array[i]);
+//   }
+// }
+
+// template <class T>
+// void iter(const T *array, size_t length, void (*func)(const T &element)) {
+//   for (size_t i = 0; i < length; i++) {
+//     func(array[i]);
+//   }
+// }
 
 #endif
--- CPP_Module_07/ex01/main.cpp
+++ CPP_Module_07/ex01/main.cpp
@@ -1,21 +1,98 @@
+// #include "iter.hpp"
+// template <class T> void printFunc(const T &element) {
+//   std::cout << element << std::endl;
+// }
+// 
+// int main(void) {
+//   {
+//     int array[5];
+// 
+//     for (int i = 0; i < 5; i++) {
+//       array[i] = i;
+//     }
+//     iter(array, 5, printFunc);
+//   }
+//   {
+//     std::string array[5] = {"A", "B", "C", "D", "E"};
+//     iter(array, 5, printFunc);
+//   }
+//   return (0);
+// }
+
+// #include "iter.hpp"
+// 
+// class Awesome
+// {
+//   public:
+//     Awesome( void ) : _n( 42 ) { return; }
+//     int get( void ) const { return this->_n; }
+//   private:
+//     int _n;
+// };
+// 
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+// {
+//   o << rhs.get();
+//   return o;
+// }
+// 
+// template< typename T >
+// void print( T& x )
+// {
+//   std::cout << x << std::endl;
+//   return;
+// }
+// 
+// int main() {
+//   int tab[] = { 0, 1, 2, 3, 4 };
+//   Awesome tab2[5];
+// 
+//   iter( tab, 5, print<const int> );
+//   iter( tab2, 5, print<Awesome> );
+// 
+//   return 0;
+// }
+
 #include "iter.hpp"
 
-template <class T> void printFunc(const T &element) {
-  std::cout << element << std::endl;
+class Awesome {
+public:
+  Awesome(void) : _n(42) { return; }
+  int get(void) const { return this->_n; }
+
+private:
+  int _n;
+};
+
+std::ostream &operator<<(std::ostream &o, Awesome const &rhs) {
+  o << rhs.get();
+  return o;
 }
 
-int main(void) {
-  {
-    int array[5];
+template <typename T> void print(T &x) {
+  std::cout << x << std::endl;
+  return;
+}
 
-    for (int i = 0; i < 5; i++) {
-      array[i] = i;
-    }
-    iter(array, 5, printFunc);
-  }
-  {
-    std::string array[5] = {"A", "B", "C", "D", "E"};
-    iter(array, 5, printFunc);
-  }
-  return (0);
+// template <typename T> void print(const T &x) {
+//   std::cout << x << std::endl;
+//   return;
+// }
+
+int main() {
+  int tab[] = {0, 1, 2, 3, 4};
+  Awesome tab2[5];
+
+  iter(tab, 5, print<const int>);
+  iter(tab2, 5, print<Awesome>);
+
+  iter(tab, 5, print<int>);
+  iter(static_cast<const int *>(tab), 5,
+       static_cast<void (*)(const int &)>(print));
+
+  // This pattern is not possible
+  //  iter(static_cast<const int *>(tab3), 5,
+  //       static_cast<void (*)(int &)>(print));
+
+  return 0;
 }
