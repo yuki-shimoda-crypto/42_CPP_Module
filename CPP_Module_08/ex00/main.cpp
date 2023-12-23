b35bc9655d92f6eac59668d757cc6ce72c61f4c2
yuki-shimoda <yuki-shimoda@protonmail.com>
2023-12-23 18:03:45 +0900

    [Module: 08] [Ex: 00] fix pusu_back to push_back
    Detailed description:
    
    Testing:

--- CPP_Module_08/ex00/main.cpp
+++ CPP_Module_08/ex00/main.cpp
@@ -3,10 +3,10 @@
 int main() {
   std::vector<int> nums;
 
-  nums.pusu_back(0);
-  nums.pusu_back(1);
-  nums.pusu_back(2);
-  nums.pusu_back(3);
+  nums.push_back(0);
+  nums.push_back(1);
+  nums.push_back(2);
+  nums.push_back(3);
   try {
     std::vector<int>::iterator it = easyfind(nums, 2);
     std::cout << "Found the number " << *it << " at index "
