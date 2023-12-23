1a6e47e860e4e0f8168ccfa3231d61a7d6bad38d
yuki-shimoda <yuki-shimoda@protonmail.com>
2023-12-23 17:38:32 +0900

    [Module: 08] [Ex: 02] Complete
    Detailed description:
    
    Testing:

--- CPP_Module_08/ex02/MutantStack.hpp
+++ CPP_Module_08/ex02/MutantStack.hpp
@@ -1,17 +1,24 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-#include <stack>
 #include <deque>
+#include <iostream>
+#include <stack>
+#include <list>
 
 template <class T, class Container = std::deque<T>>
 class MutantStack : public std::stack<T, Container> {
 public:
   typedef typename Container::iterator iterator;
+  typedef typename Container::const_iterator const_iterator;
 
-  iterator begin() { return this->c.begin(); }
+  iterator begin() { return (this->c.begin()); }
 
-  iterator end() { return this->c.end(); }
+  const_iterator begin() const { return (this->c.begin()); }
+
+  iterator end() { return (this->c.end()); }
+
+  const_iterator end() const { return (this->c.end()); }
 
 private:
 };
