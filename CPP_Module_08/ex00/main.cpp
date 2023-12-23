d93e9c39c22438645b346170639526c7d0f1e5b4
yuki-shimoda <yuki-shimoda@protonmail.com>
2023-12-23 18:02:47 +0900

    [Module: 08] [Ex: 00] Fix cannot Compile
    Detailed description:
    
    Testing:

--- CPP_Module_08/ex00/main.cpp
+++ CPP_Module_08/ex00/main.cpp
@@ -1,8 +1,12 @@
 #include "easyfind.hpp"
 
 int main() {
-  std::vector<int> nums = {0, 1, 2, 3};
+  std::vector<int> nums;
 
+  nums.pusu_back(0);
+  nums.pusu_back(1);
+  nums.pusu_back(2);
+  nums.pusu_back(3);
   try {
     std::vector<int>::iterator it = easyfind(nums, 2);
     std::cout << "Found the number " << *it << " at index "
