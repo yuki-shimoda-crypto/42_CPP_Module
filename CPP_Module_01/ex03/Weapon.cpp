8134331aa7016446fd3443a86b997a50dc928987
yuki-shimoda <yuki-shimoda@protonmail.com>
2024-02-12 15:25:45 +0900

    remove exclude cpp09

--- CPP_Module_01/ex03/Weapon.cpp
+++
@@ -1,8 +0,0 @@
-#include "Weapon.hpp"
-#include <iostream>
-
-Weapon::Weapon(std::string type) : _type(type) {}
-
-const std::string &Weapon::getType() const { return (this->_type); }
-
-void Weapon::setType(std::string type) { this->_type = type; }
